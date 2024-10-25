2b9916e640c51cf475ccb3b42490535ce44c6086
Huynh Tan Loc <12a1huynhtanloc@gmail.com>
2024-10-26 00:42:40 +0700

    env

--- product-management/index.js
+++ product-management/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const app = express();
-const port = 3000;
+require("dotenv").config();
 
 const router = require("./routers/client/index.router");
+
+const app = express();
+const port = process.env.PORT;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
