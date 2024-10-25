fc8be3a7fc6ee2e9577274df2ad91cd580328a46
Huynh Tan Loc <12a1huynhtanloc@gmail.com>
2024-10-25 13:58:37 +0700

    Done PUG

--- Bai14/vidu-01/index.js
+++ Bai14/vidu-01/index.js
@@ -67,6 +67,35 @@ app.get("/products", async (req, res) => {
   res.send("<h1>Sản phẩm</h1>");
 });
 
+app.get("/contact", (req, res) => {
+  // // const listUser = [
+  // //   {
+  // //     fullName: "Le Van A",
+  // //     email: "levana@gmail.com",
+  // //     cccd: {
+  // //       fullName: "Le Van A",
+  // //       number: "0123456789",
+  // //     },
+  // //   },
+  // //   {
+  // //     fullName: "Le Van B",
+  // //     email: "levanb@gmail.com",
+  // //   },
+  // // ];
+  // // console.log(listUser);
+  // // console.log("OK");
+  // // res.send("Hello World 123!");
+  // res.send(`
+  //   <h1>Trang chủ</h1>
+  //   <p>Tôi là Lộc</p>
+  //   `);
+  res.render("contact.pug", {
+    title: "Trang liên hệ",
+    message: "Hello there!",
+  });
+  res.send("<h1>Trang liên hệ</h1>");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
