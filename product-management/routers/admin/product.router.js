3bdcd03dee2f1c131be1f504508f580303900e5e
Huynh Tan Loc <12a1huynhtanloc@gmail.com>
2024-11-06 01:10:00 +0700

    Change upload file

--- product-management/routers/admin/product.router.js
+++ product-management/routers/admin/product.router.js
@@ -1,15 +1,20 @@
 const express = require("express");
+const multer = require("multer");
 const router = express.Router();
+const storageMulter = require("../../helpers/storageMulter");
+const upload = multer({ storage: storageMulter() });
 
 const controller = require("../../controllers/admin/product.controller");
 
 router.get("/", controller.index);
 
 router.patch("/change-status/:status/:id", controller.changeStatus);
+
 router.patch("/change-multi", controller.changeMulti);
 
 router.get("/create", controller.create);
-router.post("/create", controller.createPost);
+
+router.post("/create", upload.single("thumbnail"), controller.createPost);
 
 router.delete("/delete/:id", controller.deleteItem);
 
