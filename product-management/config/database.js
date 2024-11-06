4cb642aa3573751eb02652a48357b8ec64cbcf7a
Huynh Tan Loc <12a1huynhtanloc@gmail.com>
2024-11-07 01:44:30 +0700

    Change word

--- product-management/config/database.js
+++ product-management/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 module.exports.connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
-    console.log("Connect Succesful");
+    console.log("Connect Successful");
   } catch (error) {
     console.log("Connect Fail");
   }
