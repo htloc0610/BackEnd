897862dfdbca187e7b09196dd1561fec2f1e383a
Huynh Tan Loc <12a1huynhtanloc@gmail.com>
2024-11-06 15:58:35 +0700

    Preview Image

--- product-management/public/admin/js/product.js
+++ product-management/public/admin/js/product.js
@@ -36,3 +36,18 @@ if (buttonsDelete.length > 0) {
   });
 }
 // Finish Delete Item
+
+// Upload Image
+
+// End Upload Image
+const uploadImage = document.querySelector("[upload-image]");
+if (uploadImage) {
+  const uploadImageInput = document.querySelector("[upload-image-input]");
+  const uploadImagePreview = document.querySelector("[upload-image-preview]");
+  uploadImageInput.addEventListener("change", (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      uploadImagePreview.src = URL.createObjectURL(file);
+    }
+  });
+}
